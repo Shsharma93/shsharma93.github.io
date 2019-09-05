51e888408d422f992ed00a950fa0287c4140db98
Shashank Kumar <shsharma93@gmail.com>
2019-09-05 12:39:32 +1000

    resume updated

---
+++ app.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.typed', {
+  strings: ['Graduate Software Engineer'],
+  typeSpeed: 90,
+  startDelay: 500,
+  showCursor: false,
+  loop: false,
+  smartBackspace: true
+});
